b422a96c8856a79386099352a9334256aea8541f
Imanpal Singh <imanpal@kiprosh.com>
2022-09-17 17:25:39 +0530

    add ui for questions

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -5,5 +5,14 @@ class QuizzesController < ApplicationController
       { name: 'Krishna Singh', won: false, score: 12 },
       { name: 'Prayesh', won: false, score: 9 },
     ]
+
+    @question = {
+      "name" => 'How are turbo frames sent through the network?',
+      "answers" => [
+        'It is handled by web sockets and bla bla bla bal',
+        'It is handled by web sockets asd asd asd asd asd as das das das',
+        'It is handled by web sock',
+      ]
+    }
   end
 end
