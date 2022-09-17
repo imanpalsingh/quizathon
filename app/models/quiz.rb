2f19619d384c597301cc686afc4e6b55370a9a20
Prayesh Shah <preyashshah0805@gmail.com>
2022-09-17 17:52:12 +0530

    Generate permalink on create

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,3 +1,11 @@
 class Quiz < ApplicationRecord
   has_many :questions
+  has_many :user_quiz_sessions
+  has_many :users, through: :user_quiz_sessions
+
+  before_create :generate_unique_permalink
+
+  def generate_unique_permalink
+    self.permalink = SecureRandom.uuid
+  end
 end
