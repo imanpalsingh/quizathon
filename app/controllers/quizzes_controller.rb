1999413209fb35a8bd0674a6e3bf2a54058ef60f
Krishna Singh <krishna@kiprosh.com>
2022-09-17 19:00:11 +0530

    Add seeds for question and list all the question in index

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -6,13 +6,6 @@ class QuizzesController < ApplicationController
       { name: 'Prayesh', won: false, score: 9 },
     ]
 
-    @question = {
-      "name" => 'How are turbo frames sent through the network?',
-      "answers" => [
-        'It is handled by web sockets and bla bla bla bal',
-        'It is handled by web sockets asd asd asd asd asd as das das das',
-        'It is handled by web sock',
-      ]
-    }
+    @questions = Question.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# creats users
+User.create(first_name: 'Krishna', last_name: 'Singh', email: 'krishna@example.com', password: 'password')
+User.create(first_name: 'Imanpal', last_name: 'Singh', email: 'imanpal@example.com', password: 'password')
+User.create(first_name: 'Prayesh', last_name: 'Shah', email: 'prayesh@example.com', password: 'password')
+User.create(first_name: 'Ayush', last_name: 'Billore', email: 'ayush@example.com', password: 'password')
+
+# creats quizs
+Quiz.create(title: 'ActiveRecord')
+
+# creats user_quiz_sessions
+
+UserQuizSession.create(user_id: 1, quiz_id: 1, score: 40, active: true)
+UserQuizSession.create(user_id: 2, quiz_id: 1, score: 50, active: true)
+UserQuizSession.create(user_id: 3, quiz_id: 1, score: 60, active: true)
+UserQuizSession.create(user_id: 4, quiz_id: 1, score: 70, active: true)
+
+
+# create questions
+Question.create(quiz_id: 1, text: 'What is 2+2/2', points: 10)
+Question.create(quiz_id: 1, text: 'What is 2*2/2', points: 10)
+
+# create chioces
+Choice.create(question_id: 1, text: '2', is_correct: false)
+Choice.create(question_id: 1, text: '3', is_correct: true)
+Choice.create(question_id: 1, text: '1', is_correct: false)
+Choice.create(question_id: 1, text: '4', is_correct: false)
+
+Choice.create(question_id: 2, text: '2', is_correct: true)
+Choice.create(question_id: 2, text: '3', is_correct: false)
+Choice.create(question_id: 2, text: '1', is_correct: false)
+Choice.create(question_id: 2, text: '4', is_correct: false)
