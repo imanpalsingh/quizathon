cd070a5e6b3f2ddf1820dceedf0d2bb9c3e8a584
Prayesh Shah <preyashshah0805@gmail.com>
2022-09-17 14:03:32 +0530

    Add table and associations

---
+++ app/models/question.rb
@@ -0,0 +1,5 @@
+class Question < ApplicationRecord
+  belongs_to :quiz
+  has_many :choices
+  has_one :answer, -> { where(is_correct: true) }, class_name: 'Choice'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :user_quiz_sessions
+  has_many :quizzes, through: :user_quiz_sessions
 end
