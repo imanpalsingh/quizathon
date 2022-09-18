1f359c083f026879ca2df5439ef15eb648403a18
Krishna Singh <krishna@kiprosh.com>
2022-09-19 00:45:02 +0530

    Remove unwanted code

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'static_pages/landing_pages'
-  get 'static_pages/dashboard'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "quizzes#index"
 
   resources :quizzes, except: [:show]
