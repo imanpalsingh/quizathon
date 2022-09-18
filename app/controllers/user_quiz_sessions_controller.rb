40d3598bb7704e9ac3135cd112b0bef879efc042
Krishna Singh <krishna@kiprosh.com>
2022-09-18 23:57:45 +0530

    Validate the choices from the model

--- app/controllers/user_quiz_sessions_controller.rb
+++ app/controllers/user_quiz_sessions_controller.rb
@@ -3,14 +3,21 @@ class UserQuizSessionsController < ApplicationController
 
   def update
     user_quiz_session = UserQuizSession.find(params[:id])
-    question = user_quiz_session.quiz.questions.find(params[:user_quiz_session][:question][:id])
+    question = user_quiz_session.quiz.questions.find(uqs_params[:question][:id])
     user_quiz_session.current_question = question.id
-    is_correct? ? user_quiz_session.update_score(question.points) : user_quiz_session.touch
+    is_correct?(question) ? user_quiz_session.update_score(question.points) : user_quiz_session.touch
   end
 
   private
 
-  def is_correct?
-    params[:user_quiz_session][:question][:choice][:is_correct] == 'true'
+  def is_correct?(question)
+    choice_id = uqs_params[:question][:choice][:id]
+    question.is_choice_correct?(choice_id)
+  end
+
+  def uqs_params
+    params.require(:user_quiz_session).permit(
+      question: [:id, choice: [:id]]
+    )
   end
 end
