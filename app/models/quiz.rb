ab8c62f14e728e5901d8608bd7dc4d48dd9e15af
Krishna Singh <krishna@kiprosh.com>
2022-09-18 23:23:46 +0530

    Fix the breking functionality

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,6 +3,8 @@ class Quiz < ApplicationRecord
   has_many :user_quiz_sessions
   has_many :users, through: :user_quiz_sessions
 
+  attr_accessor :user_id
+
   before_create :generate_unique_permalink
   after_update_commit :broadcast_start_quiz, if: :saved_change_to_active?
 
@@ -12,7 +14,7 @@ class Quiz < ApplicationRecord
 
   def broadcast_start_quiz
     broadcast_replace partial: 'shared/quiz/quiz_start',
-                      locals: { question: questions.first },
+                      locals: { question: questions.first, user_quiz_session: user_quiz_session },
                       target: 'quiz-menu'
 
   end
@@ -22,5 +24,9 @@ class Quiz < ApplicationRecord
     position = questions.find(question).position
 
     questions.where("position > ?", position).first
+  end
+
+  def user_quiz_session
+    user_quiz_sessions.find_by(user_id: user_id)
   end
 end
