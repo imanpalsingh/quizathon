af0183b44b436267320f9f0cd89a5fe568a5a73d
Imanpal Singh <imanpal@kiprosh.com>
2022-09-18 14:04:34 +0530

    add copy to clipboard button

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,20 @@
+import {Controller} from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["action"]
+
+  async copy(event) {
+    event.preventDefault()
+    const [text, icon] = this.actionTarget.children
+    const orginalTextContents = icon.innerText
+
+    await navigator.clipboard.writeText(window.location.href)
+
+    icon.textContent = 'download_done'
+    if (this.timeout) clearTimeout(this.timeout)
+
+    this.timeout = setTimeout(() => {
+      icon.innerText = orginalTextContents
+    }, 1000)
+  }
+}
